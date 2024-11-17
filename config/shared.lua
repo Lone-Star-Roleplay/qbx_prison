b2975a8dbb3024191afe60fbd80ef292d6254011
James <imblazeitjames@gmail.com>
2024-11-17 17:59:20 -0600

    refactor(config): improve formatting and update coordinates for locations and spawns

--- config/shared.lua
+++ config/shared.lua
@@ -16,7 +16,7 @@ return {
                 vec3(1625.1, 2575.988, 45.56),
             },
             reward = 'phone',
-            rewardChance = 1, -- out of 100
+            rewardChance = 1,           -- out of 100
             canOnlyGetOneReward = true, -- if true, once reward is found, will not get another
         }
     },
@@ -34,52 +34,91 @@ return {
     uniforms = {
         male = {
             outfitData = {
-                ['t-shirt'] = {item = 15, texture = 0},
-                ['torso2'] = {item = 345, texture = 0},
-                ['arms'] = {item = 19, texture = 0},
-                ['pants'] = {item = 3, texture = 7},
-                ['shoes'] = {item = 1, texture = 0},
+                ['t-shirt'] = { item = 15, texture = 0 },
+                ['torso2'] = { item = 345, texture = 0 },
+                ['arms'] = { item = 19, texture = 0 },
+                ['pants'] = { item = 3, texture = 7 },
+                ['shoes'] = { item = 1, texture = 0 },
             }
         },
         female = {
             outfitData = {
-                ['t-shirt'] = {item = 14, texture = 0},
-                ['torso2'] = {item = 370, texture = 0},
-                ['arms'] = {item = 0, texture = 0},
-                ['pants'] = {item = 0, texture = 12},
-                ['shoes'] = {item = 1, texture = 0},
+                ['t-shirt'] = { item = 14, texture = 0 },
+                ['torso2'] = { item = 370, texture = 0 },
+                ['arms'] = { item = 0, texture = 0 },
+                ['pants'] = { item = 0, texture = 12 },
+                ['shoes'] = { item = 1, texture = 0 },
             }
         },
     },
 
     locations = {
         prison = {
-            {label = 'Prison', coords = vec3(1678.9, 2513.4, 45.6)},
+            { label = 'Prison', coords = vec3(1678.9, 2513.4, 45.6) },
         },
         takePhoto = {
             coords = vec3(402.9, -996.7, -100.0)
         },
         freedom = {
-            coords = vec4(1775.73, 2551.97, 44.60, 85.57)
+            coords = vector4(1840.37, 2577.76, 46.01 - 1, 356.8)
         },
         outside = {
-            coords = vec4(1848.13, 2586.05, 44.67, 269.5)
+            coords = vector4(1848.06, 2586.1, 45.67 - 1, 279.4)
         },
         yard = {
-            coords = vec4(1765.67, 2565.91, 44.56, 1.5)
+            coords = vector4(1753.83, 2501.59, 45.61 - 1, 27.63)
         },
         middle = {
-            coords = vec3(1693.33, 2569.51, 44.55)
+            coords = vector4(1693.33, 2569.51, 45.55 - 1, 123.5)
         },
         shop = {
-            coords = vec4(1752.99, 2566.99, 44.60, 231.94)
+            coords = vector4(1786.19, 2557.77, 45.62 - 1, 0.5)
         },
         spawns = {
-            { coords = vec4(1745.77, 2489.61, 50.41, 212.01), animation = 'bumsleep' },
-            { coords = vec4(1751.86, 2492.73, 50.44, 213.77), animation = 'lean' },
-            { coords = vec4(1760.82, 2498.14, 50.42, 208.26), animation = 'sitchair4' },
-            { coords = vec4(1754.85, 2494.59, 45.82, 212.49), animation = 'finger2' },
-            { coords = vec4(1748.84, 2491.35, 45.80, 203.43), animation = 'smoke' }
+            [1] = {
+                animation = "bumsleep",
+                coords = vector4(1661.046, 2524.681, 45.564 - 1, 260.545)
+            },
+            [2] = {
+                animation = "lean",
+                coords = vector4(1650.812, 2540.582, 45.564 - 1, 230.436)
+            },
+            [3] = {
+                animation = "lean",
+                coords = vector4(1654.959, 2545.535, 45.564 - 1, 230.436)
+            },
+            [4] = {
+                animation = "lean",
+                coords = vector4(1697.106, 2525.558, 45.564 - 1, 187.208)
+            },
+            [5] = {
+                animation = "sitchair4",
+                coords = vector4(1673.084, 2519.823, 45.564 - 1, 229.542)
+            },
+            [6] = {
+                animation = "sitchair",
+                coords = vector4(1666.029, 2511.367, 45.564 - 1, 233.888)
+            },
+            [7] = {
+                animation = "sitchair4",
+                coords = vector4(1691.229, 2509.635, 45.564 - 1, 52.432)
+            },
+            [8] = {
+                animation = "finger2",
+                coords = vector4(1770.59, 2536.064, 45.564 - 1, 258.113)
+            },
+            [9] = {
+                animation = "smoke",
+                coords = vector4(1792.45, 2584.37, 45.56 - 1, 276.24)
+            },
+            [10] = {
+                animation = "smoke",
+                coords = vector4(1768.33, 2566.08, 45.56 - 1, 176.83)
+            },
+            [11] = {
+                animation = "smoke",
+                coords = vector4(1696.09, 2469.4, 45.56 - 1, 1.4)
+            }
         }
     },
 
